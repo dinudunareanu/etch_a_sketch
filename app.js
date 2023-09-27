cff4410df0c9931f124ac7e55ec08f60be49e43a
dinudunareanu <dunareanudinu@gmail.com>
2023-09-27 18:31:07 +0300

    Started working on drawing function

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const container = document.querySelector('.container');
 const square = document.createElement('div');
 square.classList.add('square');
 
+const isSpacePressed = false;
+
+document.body.addEventListener('keydown', (x) => {
+    if (x.code === "Space") {
+        isSpacePressed = true;
+    }
+});
+
+document.body.addEventListener('keyup', (x) => {
+    console.log(x);
+});
+
 for (let i = 0; i < 16 * 16; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
+    square.addEventListener('mouseenter', () => {
+        square.classList.add("black");
+    });
 }
