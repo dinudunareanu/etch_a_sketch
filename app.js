18ac7d990bd76c918fa390b3cbf6e3e66565a3aa
dinudunareanu <dunareanudinu@gmail.com>
2023-09-28 16:28:45 +0300

    Generate board with a given number of squares

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const container = document.querySelector('.container');
 const square = document.createElement('div');
-square.classList.add('square');
+const genBtn = document.querySelector('#genBtn');
 
-const isSpacePressed = false;
+let pressed = false;
 
-document.body.addEventListener('keydown', (x) => {
-    if (x.code === "Space") {
-        isSpacePressed = true;
+container.addEventListener('click', () => {
+    pressed = !pressed;
+});
+
+genBtn.addEventListener('click', () => {
+    //reset board
+    document.querySelector('.container').innerHTML = '';
+    //generate board
+    
+    let numberOfSquares = document.querySelector('#gen').value;
+    console.log(numberOfSquares);
+    if (numberOfSquares === "0" || numberOfSquares === "" || numberOfSquares > 250) {
+        alert("Invalid input!");
+    }
+    else {
+        let squareWidth = 700 / numberOfSquares;
+    
+        for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+            let square = document.createElement('div');
+            square.style.width = `${squareWidth}px`;
+            square.style.height = `${squareWidth}px`;
+            container.appendChild(square);
+            square.addEventListener('mouseover', () => {
+                if (pressed) {
+                    square.classList.add("black");
+                }
+            });
+        }
+
+        alert("Board generated!");
     }
 });
-
-document.body.addEventListener('keyup', (x) => {
-    console.log(x);
-});
-
-for (let i = 0; i < 16 * 16; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-    square.addEventListener('mouseenter', () => {
-        square.classList.add("black");
-    });
-}
